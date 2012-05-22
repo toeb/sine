496996f7a70f528e97dca803710eb3a32aa0c995
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-05-22 20:43:59 +0000

    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@71 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- Simulation/DynamicsAlgorithm.cpp
+++ Simulation/DynamicsAlgorithm.cpp
@@ -19,26 +19,12 @@ IIntegrable & DynamicsAlgorithm::getIntegrable(){
 }
 
 void DynamicsAlgorithm::evaluate(Real t, Real h){
-  //this does not have to be done every time.  
-
-  /*/ calculates the inverted inertia tensor in world coordinates at time t
-  // also calculates the rotation and inverted rotation matrices
   dynamicBodyModule.calculateCachedValues(); 
-  //precomputes the connector positions
   connectorModule.calculateConnectorPositions();
-  // sets the forces for each body
-  forceModule.setForces(t);//*/  
-  //calculates the accelerations of the dynamic bodies (evaluates the mass equations)
-  dynamicBodyModule.calculateCachedValues(); 
-  //precomputes the connector positions
-  connectorModule.calculateConnectorPositions();
-  
   forceModule.setForces(t);
   dynamicBodyModule.calculateDynamics();
 }
 void DynamicsAlgorithm::preIntegration(Real t, Real h){
-  
-  // sets the forces for each body
   multiBodyDynamics.correctPositions(h);
 }
 void DynamicsAlgorithm::postIntegration(Real t,Real h){
