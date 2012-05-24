780249e1d593b3646e361c2674c65e1ebe806855
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-05-24 15:49:10 +0000

    added generic classes for values
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@80 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

---
+++ Visualization/UserInterface/DelegateValue.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Simulation\ISimulationObject.h>
+#include <Visualization/UserInterface/IValue.h>
+#include <Visualization\UserInterface\IComponent.h>
+#include <functional>
+namespace IBDS{
+
+/**
+ * \class DelegateValue
+ * 				allows easy getter setter functionality for any value specific by type argument T.  
+ * 				expects a std::function as  a getter and a setter
+ *
+ * \brief Delegate value.
+ */
+template<class T>
+class DelegateValue : public virtual IValue{
+private:
+  std::function<void (T)> _setter;
+  std::function<T ()> _getter;
+public:
+  DelegateValue(const std::string & name, std::function<T()> getter, std::function<void (T)> setter);
+  void set(const void * value);
+  void get(void * value);
+};
+}
