6aae2910f8c4488265b954aeedb0c6b6e5ab556d
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-09-20 13:30:56 +0000

    forgot to add config.h
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@240 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

---
+++ src/config.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifndef nspace
+#define nspace ds
+#endif
+
+#include <float.h>//for DBL_MAX
+
+typedef unsigned int uint;
+typedef unsigned char byte;
+
+// define the floating point type Real
+#ifdef USE_DOUBLES
+	typedef double Real;
+	#define REAL_MAX DBL_MAX
+	#define REAL_MIN DBL_MIN
+#else
+	typedef float Real;
+
+	#define REAL_MAX FLT_MAX
+	#define REAL_MIN FLT_MIN
+#endif
+  
+#define EPSILON 1.0E-9
+#define PI 3.14159  
+
+#define ERRORSTREAM std::cerr
+#ifndef WIN32 
+#define __FUNCSIG__ __PRETTY_FUNCTION__
+#endif
+#define ERROR(x) ERRORSTREAM << "Error '"<<x<<"' in " << __FUNCSIG__ << " at line "<<__LINE__<<std::endl;
