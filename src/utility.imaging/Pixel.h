cfc226770d9934b1e6158f3d82b45aff4d7f19ea
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-09-21 19:42:55 +0000

    added Image and Pixel
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@262 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

---
+++ src/utility.imaging/Pixel.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <config.h>
+
+namespace nspace{
+
+  union Pixel{
+    struct{
+      byte r;
+      byte g;
+      byte b;
+      byte a;
+    }color;
+    byte value[4];
+    byte toGrayscale()const{
+      return (byte) (0.2126 * color.r + 0.7152 * color.r + 0.0722 * color.b);
+    }
+    void fromGrayscale(byte grayscale){
+      color.r = grayscale;
+      color.g = grayscale;
+      color.b = grayscale;
+      color.a = 255;
+    }
+  };
+
+}
