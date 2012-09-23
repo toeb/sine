6c4f7fabc11e211a1abc0972fda14045d20a3f22
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-09-23 14:01:54 +0000

    commented State class
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@275 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- src/simulation.state/State.h
+++ src/simulation.state/State.h
@@ -8,40 +8,54 @@ namespace nspace{
 
   typedef MatrixNxM StateMatrix;
 
+  // represents the state and its derivatives.
+  // this class manages the vectors for each derivative by putting them into one large matrix
+  // state is a recursive datatype allowing substates which have the same data as the parent state
+  // TODO: error checking needs to be done so that no unintended behaviour occurs when the state is empty
   class State{
     State * _parent;
     StateMatrix *_stateMatrix;
     uint _offset;
+    // number of state vars = dimension of state
     uint _dimension;
+    // number of derivatives x => 1;  x xDot => 2; x, xDot, xDotDot => 3 
     uint _derivatives;
 
-    State(uint offset, uint dimension, uint derivatives, State & parent):_parent(&parent),_offset(offset),_derivatives(derivatives),_stateMatrix(parent._stateMatrix),_dimension(dimension){
-
-    }
+    // private constructor
+    State(uint offset, uint dimension, uint derivatives, State & parent);
   public:
-
-    State():_parent(0),_derivatives(0),_offset(0),_dimension(0),_stateMatrix(0){
-
-    }
+    // public constructor initializes state to an empty state
+    // clients need to resize it to their needs
+    State();
 
     ~State(){
       //if(!_parent)delete &_stateMatrix;
     }
+
+    // the number of derivatives
     inline uint derivatives()const{return _derivatives;}
+    // the dimension
     inline uint dimension()const{return _dimension;}
+
+    // matrix style access
     inline Real & operator()(uint stateIndex, uint derivative){
       return _stateMatrix->operator()(_offset+stateIndex,derivative);
     }
+
+    // matrix style const access
     inline const Real & operator()(uint stateIndex, uint derivative)const{
       return _stateMatrix->operator()(_offset+stateIndex,derivative);
     }
 
-    Real * stateVector(uint derivative){
+    // returns the data pointer to the state vector
+    inline Real * stateVector(uint derivative){
       Real * data = _stateMatrix->data();
       return data +(derivative*_stateMatrix->cols()+_offset);
       //return _stateMatrix->rowData(derivative)+_offset;
     }
-    void getState(StateMatrix & state){      
+
+    // files the state matrix with the state ( it will resize the matrix accordingly)
+    inline void getState(StateMatrix & state){      
       if(_derivatives == 0 && _dimension==0)return;
       state.resize(_derivatives,_dimension);
       //state.resize(_derivatives,_dimension,false);
@@ -49,7 +63,9 @@ namespace nspace{
       //state = _stateMatrix->block(0,_offset,_derivatives,_dimension);
       //_stateMatrix->getBlock(state,0,_offset);
     }
-    void setState(StateMatrix & state){
+
+    // sets the state to the values of the statematrix passed
+    inline void setState(StateMatrix & state){
       if(state.rows()!=derivatives() || state.cols()!=dimension()){
         std::cerr << "State::Could not set state " << std::endl;
         return;
@@ -59,7 +75,10 @@ namespace nspace{
       //_stateMatrix->block(0,_offset,_derivatives,_dimension) = state;
       //_stateMatrix->setBlock(0,_offset,state);
     }
-    void getX(StateMatrix & x)const{
+
+    // gets the 0th derivative
+    void getX(StateMatrix & x)const{      
+      if(_derivatives==0){ ERROR("There are no derivatives present"); return ;}
       //x.resize(_derivatives-1,_dimension,false);
       x.resize(_derivatives-1,_dimension);
       x.setZero();
@@ -67,15 +86,20 @@ namespace nspace{
       matrix::getBlock(x,*_stateMatrix,0,_offset);
       //_stateMatrix->getBlock(x,0,_offset);
     }
+
+    // gets the first derivative
     void getXDot(StateMatrix & xDot)const{
       //xDot.resize(_derivatives-1,_dimension,false);
       //_stateMatrix->getBlock(xDot,1,_offset);
+      if(_derivatives==0){ ERROR("There are no derivatives present"); return ;}
       xDot.resize(_derivatives-1,_dimension);
       xDot.setZero();
       matrix::getBlock(xDot,*_stateMatrix,1,_offset);
       //x = _stateMatrix->block(1,_offset,_derivatives-1,_dimension);
       
     }
+
+    // sets the 0th derivative
     void setX(const StateMatrix & x){
       if(x.rows()!=derivatives()-1 || x.cols()!=dimension()){
         std::cerr << "State::Could not set state " << std::endl;
@@ -84,7 +108,7 @@ namespace nspace{
       matrix::setBlock(*_stateMatrix,x,0,_offset);
       //_stateMatrix->setBlock(0,_offset,x);
     }
-
+    // resizes the state
     void resize(uint newDim, uint newDerivs){
       if(!_stateMatrix )_stateMatrix = new StateMatrix();
       if(_parent)return;
@@ -93,11 +117,14 @@ namespace nspace{
       _stateMatrix->resize(newDerivs,newDim);
       //_stateMatrix->resize(newDerivs,newDim,false);
     }
+
+    // returns a state range 
     State * range(uint offset, uint dim ,uint derivatives){
       State * result = new State(offset,dim,derivatives,*this);
       return result;
     }
   };
 
+  // prints the state
   std::ostream & operator << (std::ostream & o, const State & state);
 }
