a0a576ad28faedfee9ffb16def62e3e51c0cc6b0
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-09-23 13:59:30 +0000

    commented set and node, moved implementation to separte file resp. to end of header
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@274 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- src/core/Node.h
+++ src/core/Node.h
@@ -7,193 +7,102 @@
 
 namespace nspace{
 
-/**
- * \brief Node.forward declaration for comparator
- */
-class Node;
+  /**
+  * \brief Node.forward declaration for comparator
+  */
+  class Node;
 
-/**
- * \brief Comparator specialization. for comparing a const char * to a node
- *
- * \tparam  Node *        Type of the node *.
- * \tparam  const char *  Type of the constant character *.
- */
-template<> class Comparator< Node *, const char *>{
-public:
-  static void operation(bool & result, const Node * a, const char * b);
-};
+  /**
+  * \brief Comparator specialization. for comparing a const char * to a node
+  *
+  * \tparam  Node *        Type of the node *.
+  * \tparam  const char *  Type of the constant character *.
+  */
+  template<> class Comparator< Node *, const char *>{
+  public:
+    static void operation(bool & result, const Node * a, const char * b);
+  };
 
 
 
   /**
-   * \brief Directional node. can have multiple predecessors as well as successors
-   * 				
-   * 				utilises set
-   */
+  * \brief Directional node. can have multiple predecessors as well as successors
+  * 				
+  * 				utilizes set
+  */
   class Node : public ValueLookup, public NamedObject, public ObservableCollection<Node*>, public ObservableCollection<Node*>::Observer{
   private:
+    // stores all direct predecessors
     Set<Node*> _predecessors;
+    // stores all direct successors
     Set<Node*> _successors;
   public: 
-    Node(){
-      _predecessors.addObserver(this);
-      _successors.addObserver(this);
-    }
+    // creates a unnamed node
+    Node();
     // creates  named node
-    Node(const std::string & name){
-      setName(name);
-      _predecessors.addObserver(this);
-      _successors.addObserver(this);
-    }
-
+    Node(const std::string & name);
     // destroys node. causing all connected nodes to be disconnected
-    ~Node(){_predecessors.clear(); _successors.clear();}
-
+    ~Node();
     // all connected nodes (union of predecessors and successors)
-    Set<Node*> neighbors()const{return predecessors()|successors();};
-    
-    
-    const Set<Node*> & predecessors()const{
-      return _predecessors;
-    }
-    
-    Set<Node*> & predecessors(){
-      return _predecessors;
-    }
+    Set<Node*> neighbors()const;
+    // read access to predecessors
+    const Set<Node*> & predecessors()const;
+    // read write access to predecessors
+    Set<Node*> & predecessors();
+    // read write access to successor by index
+    Node * successor(uint i);
+    // read access to successor by index
+    const Node * successor(uint i)const;
+    //read / write access to predecessor by index
+    Node * predecessor(uint i);
+    // read access to predecessor by index
+    const Node * predecessor(uint i)const;
+    // read access tot first successor
+    const Node * firstSuccessor()const;
+    //read write access to first successor
+    Node * firstSuccessor();
+    // returns the first predecessor (const)
+    const Node * firstPredecessor()const;    
+    // returns the first predecessor
+    Node * firstPredecessor();
+    // allows const access to the successors
+    const Set<Node*> & successors()const;
+    //allows access to the successors
+    Set<Node*> & successors();
+
+    // adds a set of predecessors (arrows indicate direction of connection
+    Node & operator <<(const Set<Node*> & nodes);
+    // adds a set of successors 
+    Node & operator >>(const Set<Node*> & nodes);
+    // adds a single predecessor
+    Node & operator <<(Node & node);
+    //adds a single successor
+    Node & operator >>(Node & node);
+    // adds a single predecessor (by pointer)
+    Node & operator <<(Node * node);
+    // adds a single successor (by pointer)
+    Node & operator >>(Node * node);
 
 
-    const Set<Node*> & successors()const{
-      return _successors;
-    }
+    // overload for dfs(f,successors)
+    void dfs(std::function<void (bool & , Node *)> f);
+    // depth first search order
+    void dfs(std::function<void (bool & , Node *)> f, std::function<void (Set<Node*> &, const Node &) > successors);
 
-     Set<Node*> & successors(){
-      return _successors;
-    }
-
-     // adds a set of predecessors (arrows indicate direction of connection
-     Node & operator <<(const Set<Node*> & nodes){
-       predecessors() |= nodes;
-       return *this;
-     }
-     // adds a set of successors 
-     Node & operator >>(const Set<Node*> & nodes){
-       successors() |= nodes;
-       return *this;
-     }
-     // adds a single predecessor
-     Node & operator <<(Node & node){
-       predecessors().add(&node);
-       return *this;
-     }
-     //adds a single successor
-     Node & operator >>(Node & node){
-       successors().add(&node);
-       return *this;
-     }
-     // adds a single predecessor (by pointer)
-     Node & operator <<(Node * node){
-       predecessors().add(node);
-       return *this;
-     }
-     // adds a single successor (by pointer)
-     Node & operator >>(Node * node){
-       successors().add(node);
-       return *this;
-     }
-     // overload for dfs(f,successors)
-     void dfs(std::function<void (bool & , Node *)> f){
-       dfs(f,[](Set<Node *>& successors,const Node & node){ successors |= node.successors(); });
-     }
-    // really bad implementation of dfs, it is slow and will crash if there are cycles in the graph
-    void dfs(std::function<void (bool & , Node *)> f, std::function<void (Set<Node*> &, const Node &) > successors){
-      
-      Set<Node* > next;
-      successors(next,*this);
-      bool cont=true;
-      f(cont,this);
-      if(!cont)return;
-      next.foreachElement([f,successors](Node * n){
-        n->dfs(f,successors);
-      });      
-    }
-
-    // also a realy bad implemention. this time of bfs (performancewise). it does not crash if cycles are contained and returns the number of cycles found
-    int bfs(std::function<void (bool& ,Node*)> f,std::function<void (Set<Node*> &, const Node &) > successors=0){
-      if(!successors)successors = [](Set<Node *>& successors,const Node & node){
-        successors |= node.successors();
-      };
-
-       
-       Set<Node *> list = this;
-       int cycles =0;
-       while(list){
-         // get first element
-         Node * current = list.first();
-         // remove first element
-         list /= current;
-         
-         //execute function
-         bool cont = true;
-         f(cont,current);
-         if(!cont)return cycles;
-
-         // add successors of current;
-         successors(list,*current);
-       }
-       return cycles;
-     }
+    // breadth first search order (performancewise). returns the number of cycles found
+    // successorFunction shall return the successors for the node ( for example let it return the predecessors if bfs is to be performed
+    // on the predecessors
+    int bfs(std::function<void (bool& ,Node*)> f,std::function<void (Set<Node*> &, const Node &) > successorFunction);
+    // overload for bfs
+    int bfs(std::function<void (bool& ,Node*)> f);
 
     // prints the suceeding graph to the output stream reference
-     friend std::ostream &  operator << (std::ostream & c, Node& node){
-      node.set("level",0);
-       node.dfs([&c](bool & cont, Node * n){
-         int level = n->get<int>("level");
-
-         for(int j=0; j < level*2; j++)c <<" ";
-
-         c <<n->name() <<std::endl;
-         
-         for(unsigned int i=0; i < n->entries();i++){
-           Entry * entry = n->entries()(i);
-           for(int j=0; j < level*2+1; j++)c <<" ";
-           c << entry->name << std::endl; 
-         }
-
-         for(unsigned int i=0; i < n->successors(); i++){
-           n->successors()(i)->set("level",level+1);
-         }
-
-      });
-      return c;
-     }
+    friend std::ostream &  operator << (std::ostream & c, Node& node);
   protected:
     // callback when an element is added to the node. makes sure that the nodes are connected in both directions
-    void elementAdded(ObservableCollection<Node*> * sender, Node * node){
-      if(sender == &_predecessors){
-        // makes sure that the new nodes successor is set to this
-        node->successors().add(this);
-        // call extension point
-        onPredecessorAdded(node);
-      }else
-      if(sender==&_successors){
-        // make sure that the new nodes predecessor is set to this
-        node->predecessors().add(this);
-        onSuccessorAdded(node);
-      }
-      notifyElementAdded(node);
-    }
+    void elementAdded(ObservableCollection<Node*> * sender, Node * node);
     // callback when an element is removed from the node. makes sure the nodes are removed
-    void elementRemoved(ObservableCollection<Node*> * sender, Node * node){
-      if(sender == &_predecessors){
-        node->successors().remove(this);
-        onPredecessorRemoved(node);
-      }else
-      if(sender==&_successors){
-        node->predecessors().remove(this);
-        onSuccessorRemoved(node);
-      }
-      notifyElementRemoved(node);
-    }
+    void elementRemoved(ObservableCollection<Node*> * sender, Node * node);
     virtual void onPredecessorAdded(Node * predecessor){}
     virtual void onSuccessorAdded(Node * successor){}
     virtual void onPredecessorRemoved(Node* predecessor){}
--- src/core/Set.h
+++ src/core/Set.h
@@ -8,189 +8,306 @@
 #include <core/Query.h>
 #include <core/ObservableCollection.h>
 
-
 namespace nspace{
 
-template<typename T>
-class Set : public ObservableCollection<T>{   
-public:
-  //the underlying element vector
-  std::vector<T> _elements;
- public:
- 
-   operator unsigned int() const {
-     return size();
-   }
+  // a generic set (only unique elements allowed)
+  template<typename T>
+  class Set : public ObservableCollection<T>{   
+  public:
+    //the underlying element vector
+    std::vector<T> _elements;
+  public:
+    // creates a copy from another set
+    Set(const Set<T> & original);
+    // creates a set from a single element
+    Set(T element);
+    //creates an empty set
+    Set();
+    // destroys the set
+    ~Set();
 
-  T first(){
+    // removes all elements from the set
+    void clear();
+    // adds a element to the set
+    virtual bool add(T element);
+    // removes a element from the set
+    virtual bool remove(T element);
+
+    // returns cardinality  (size of the set)
+    operator unsigned int() const;
+    // returns true iff set is empty (size==0)
+    bool empty()const;
+    // returns the size of the set
+    uint size()const;
+
+
+    // returns the first element
+    T first();
+    const T  first()const;
+    // returns the element at index
+    T  at(unsigned int index);
+    const T at(unsigned  int index)const;
+    // returns the element at index
+    T operator()(unsigned int index);
+    const T operator()(unsigned int index)const;
+    // read access to the std::vector of elements 
+    const std::vector<T> & elements()const;
+
+    // returns the first element matching the Compartype
+    template<typename CompareType> T operator()(CompareType val)const;
+    // returns the subset of elements where the predicate evaluates to true
+    Set<T> select(std::function<bool (T)> predicate)const;
+    // returns the first element were the predicate evaluates to true
+    T selectFirst(std::function<bool (T)> f)const;
+    // copies every element were the predicate evaluates to true to the result set
+    void selectInto(Set<T> & result, std::function<bool (T) > predicate)const;
+    // copies every element from other set to this set
+    void copyFrom(const Set<T> & other);
+    // copies every element from this set to the other set
+    void copyTo(Set<T> & result);
+    // returns true if the element is part of the set
+    bool contains(T  element)const;
+
+
+    // reduce into result
+    template<typename Result> void reduce(Result & result, std::function<void (Result & , T )> f)const;
+    // executes action on each element
+    void foreachElement(std::function<void(T)> action)const;
+
+    // assigns the elements of another set to this set discarding all current content
+    Set<T> & operator=(const Set<T>& a);
+    //union
+    Set<T> operator|(const Set<T> &b)const;
+    //difference
+    Set<T> operator/(const Set<T> & b)const;
+    // comma  extends the set by the elements of set b ( |= )
+    Set<T> & operator,(const Set<T> & b);
+    // exten set by elements of set b
+    Set<T> & operator |=(const Set<T> & b);
+    // removes all elements from set which are also contained in set b (difference equals)
+    Set<T> & operator /=(const Set<T> & b);
+    // returns the symmetric difference (xor)
+    Set<T> operator ^(const Set<T> & b)const;
+    // returns the intersection of this set and set b;
+    Set<T> operator &(const Set<T> & b)const;
+    // returns true if the sets contain exactly the same elements (symmetric difference has a cardinality of zero)
+    bool operator ==(const Set<T> & b)const;
+
+    //static are equal test
+    static bool areEqual(const Set<T> &a, const Set<T> & b);
+    // static symmetric difference 
+    static Set<T> symetricDifference(const Set<T> &a , const Set<T> & b);
+    // static difference
+    static Set<T> difference(const Set<T> & a, const Set<T> & b);
+    // static union
+    static Set<T> unite(const Set<T> & a, const Set<T> & b);
+    // static intersection
+    static Set<T> intersect(const Set<T> & a, const Set<T> & b);
+  };
+
+  //IMPLEMENTATION FOLLOWS:
+
+
+  // returns cardinality  (size of the set)
+  template<typename T>
+  Set<T>::operator unsigned int() const {
+    return size();
+  }
+
+  // returns the first element
+  template<typename T>
+  T Set<T>::first(){
     return at(0);
   }
-  const T  first()const{
+
+  template<typename T>
+  const T  Set<T>::first()const{
     return at(0);
   }
-  T  at(unsigned int index){
+  template<typename T>
+  T  Set<T>::at(unsigned int index){
     if(index >= size())return 0;
     return _elements[index];
   }
-  const T at(unsigned  int index)const{
+  template<typename T>
+  const T Set<T>::at(unsigned  int index)const{
     if(index >= size())return 0;
     return _elements[index];
   }
+  template<typename T>
+  T Set<T>::operator()(unsigned int index){
+    return at(index);
+  }
+  template<typename T>
+  const T Set<T>::operator()(unsigned int index)const{
+    return at(index);
+  }
 
-  T operator()(unsigned int index){
-    return at(index);
-  }
-  const T operator()(unsigned int index)const{
-    return at(index);
-  }
-  
+  template<typename T>
   template<typename CompareType>
-  T operator()(CompareType val)const{
+  T Set<T>::operator()(CompareType val)const{
     return selectFirst([&val](T element){
       return compare(element,val);
     });
   }
-  
-  bool empty()const{
+
+
+  template<typename T>
+  bool Set<T>::empty()const{
     return size()==0;
   }
-  uint size()const{
+  template<typename T>
+  uint Set<T>::size()const{
     return _elements.size();
   }
-  const std::vector<T> & elements()const{return _elements;}
-  
-  void clear(){
+  template<typename T>
+  const std::vector<T> & Set<T>::elements()const{return _elements;}
+  template<typename T>
+  void Set<T>::clear(){
     std::vector<T> elements = _elements;
     std::for_each(elements.begin(), elements.end(), [this](T element){
       this->remove(element);
     });
   }
-
-  virtual bool add(T element){
+  template<typename T>
+  bool Set<T>::add(T element){
     if(contains(element))return false;
     _elements.push_back(element);
     notifyElementAdded(element);
   }
-  virtual bool remove(T element){
+  template<typename T>
+  bool Set<T>::remove(T element){
     bool result = Query<T>::remove(_elements,element);
     if(result)notifyElementRemoved(element);
     return result;
   }
-  void copyFrom(const Set<T> & result){
+  template<typename T>
+  void Set<T>::copyFrom(const Set<T> & result){
     result.foreachElement([this](T t){
       this->add(t);
     });
   }
-  void copyTo(Set<T> & result){
+  template<typename T>
+  void Set<T>::copyTo(Set<T> & result){
     foreachElement([&result](T t){
       result.add(t);
     });
   }
-  void selectInto(Set<T> & result, std::function<bool (T) > f)const{
+  template<typename T>
+  void Set<T>::selectInto(Set<T> & result, std::function<bool (T) > f)const{
     Query<T>::select(result._elements,_elements,[f](bool & predicate, bool & cont, T elem){
       predicate = f(elem);
     });
   }
-  bool contains(T  element)const{
+  template<typename T>
+  bool Set<T>::contains(T  element)const{
     return selectFirst([element](T e){return e==element;})!=0;
   }
-  
 
-  Set<T> select(std::function<bool (T)> f)const{
+  template<typename T>
+  Set<T> Set<T>::select(std::function<bool (T)> f)const{
     Set<T> result;
     selectInto(result,f);
     return result;
   }
-  
-  T selectFirst(std::function<bool (T)> f)const{
+  template<typename T>
+  T Set<T>::selectFirst(std::function<bool (T)> f)const{
     return Query<T>::selectFirst(_elements,f);
   } 
 
+  template<typename T>
   template<typename Result>
-  void reduce(Result & result, std::function<void (Result & , T )> f)const{
+  void Set<T>::reduce(Result & result, std::function<void (Result & , T )> f)const{
     foreachElement([&result,f](T elem){
       f(result,elem);
     });
   }
 
-  
-  void foreachElement(std::function<void(T)> f)const{
+  template<typename T>
+  void Set<T>::foreachElement(std::function<void(T)> f)const{
     std::for_each(elements().begin(),elements().end(),f);
   }
-
-  Set<T> & operator=(const Set<T>& a){
+  template<typename T>
+  Set<T> & Set<T>::operator=(const Set<T>& a){
     if(&a==this)return*this;
     clear();
     copyFrom(a);
     return *this;
   }
-
-  Set(const Set<T> & original){
+  template<typename T>
+  Set<T>::Set(const Set<T> & original){
     copyFrom(original);
   }
-
-  Set(T element){
+  template<typename T>
+  Set<T>::Set(T element){
     add(element);
   }
 
-
-  Set(){
+  template<typename T>
+  Set<T>::Set(){
 
   }
-
-  ~Set(){
+  template<typename T>
+  Set<T>::~Set(){
     clear();
   }
 
 
   //union
-  Set<T> operator|(const Set<T> &b)const{
+  template<typename T>
+  Set<T> Set<T>::operator|(const Set<T> &b)const{
     return unite(*this,b);
   }
 
-  
-  
+
+
   //difference
-  Set<T> operator/(const Set<T> & b)const{
+  template<typename T>
+  Set<T> Set<T>::operator/(const Set<T> & b)const{
     return difference(*this,b);
   }
-
-  Set<T> & operator,(const Set<T> & b){    
+  template<typename T>
+  Set<T> & Set<T>::operator,(const Set<T> & b){    
     *this |= b;
     return *this;
   }
-
-  Set<T> & operator |=(const Set<T> & b){
+  template<typename T>
+  Set<T> & Set<T>::operator |=(const Set<T> & b){
     b.reduce<Set<T> >(*this,[](Set<T> & accu,T element){
       accu.add(element);
     });
     return *this;
   }
-  Set<T> & operator /=(const Set<T> & b){
+  template<typename T>
+  Set<T> & Set<T>::operator /=(const Set<T> & b){
     b.reduce<Set<T> >(*this,[](Set<T> & accu,T element){
       accu.remove(element);
     });
     return *this;
   }
-  
 
-  Set<T> operator ^(const Set<T> & b)const{
+  template<typename T>
+  Set<T> Set<T>::operator ^(const Set<T> & b)const{
     return symetricDifference(*this, b);
   }
-  Set<T> operator &(const Set<T> & b)const{
+  template<typename T>
+  Set<T> Set<T>::operator &(const Set<T> & b)const{
     return intersect(*this,b);
   }
-  bool operator ==(const Set<T> & b)const{
+  template<typename T>
+  bool Set<T>::operator ==(const Set<T> & b)const{
     return areEqual(*this,b);
   }
-
-  static bool areEqual(const Set<T> &a, const Set<T> & b){
+  template<typename T>
+  bool Set<T>::areEqual(const Set<T> &a, const Set<T> & b){
     return symetricDifference(a,b).size()==0;
   }
-  static Set<T> symetricDifference(const Set<T> &a , const Set<T> & b){
+  template<typename T>
+  Set<T> Set<T>::symetricDifference(const Set<T> &a , const Set<T> & b){
     return unite(difference(a,b),difference(b,a));
   }
-  static Set<T> difference(const Set<T> & a, const Set<T> & b){
+  template<typename T>
+  Set<T> Set<T>::difference(const Set<T> & a, const Set<T> & b){
     Set<T> result;
     result.copyFrom(a);
     b.reduce<Set<T> >(result,[](Set<T> & accu, T element){
@@ -198,7 +315,8 @@ public:
     });
     return result;
   }
-  static Set<T> unite(const Set<T> & a, const Set<T> & b){
+  template<typename T>
+  Set<T> Set<T>::unite(const Set<T> & a, const Set<T> & b){
     Set<T> result;
     result.copyFrom(a);
     b.reduce<Set<T> >(result,[](Set<T>& accu, T element){
@@ -206,13 +324,12 @@ public:
     });
     return result;
   }
-  static Set<T> intersect(const Set<T> & a, const Set<T> & b){
+  template<typename T>
+  Set<T> Set<T>::intersect(const Set<T> & a, const Set<T> & b){
     Set<T> result;
     b.reduce<Set<T> >(result,[&a](Set<T> & accu, T element){
       if(a.contains(element))accu.add(element);
     });
   }
-  };
 
-  
 }
