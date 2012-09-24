5ae4fd98332f0131d39120ec6d502a638edeae81
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-09-24 10:05:25 +0000

    moved implementation to cpp files all models created are now accessible trhough set models()
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@296 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- src/simulation.model/builder/ModelBuilderBase.h
+++ src/simulation.model/builder/ModelBuilderBase.h
@@ -8,66 +8,39 @@ namespace nspace{
 
   class ModelBuilderBase : public IModelBuilder, public Set<ModelElement*> {
   private:  
+    Set<Model*> _models;
     Model * _currentModel;
     ModelElement * _currentElement;
   protected:
-    virtual Node * createNode(){return new Node;};
-    virtual Connection * createConnection(){return new Connection;}
-    virtual Connector * createConnector(){return new Connector;}
+    virtual Node * createNode();
+    virtual Connection * createConnection();
+    virtual Connector * createConnector();
   public:    
     virtual void buildModel(Model & model)=0;
-  
 
-    ModelBuilderBase(){
-      reset();
-    }
-    void reset(){
-      _currentModel=0;
-      _currentElement=0;
-    }
-    Model * currentModel(){
-      return _currentModel;
-    }
-    Model * beginModel(){
-      _currentModel = new Model();            
-      addElement(_currentModel);
-      return _currentModel;
-    }
-    Model * endModel(){
-      buildModel(*_currentModel);
-      Model * model = _currentModel;
-      _currentModel =0;
-      return model;
-    }
-    Node * addNode(){
-      Node * node = createNode();
-      //addElement(node);
-      _currentModel->nodes().add(node);
-      return node;   
-    }
-    Connector * addConnector(const std::string & nodeName){
-      return 0;
-    }
-    Connection * addConnection(const std::string & nodeAm, const std::string & nodeB){
-      Connection * connection = createConnection();
-   //   addElement(connection);
-//      _currentModel->connections().add(connection);
-      return connection;
-    }
+    ModelBuilderBase();
+    void reset();
+    const Set<Model*> models()const;
+    Model * currentModel();
+    Model * beginModel();
+    Model * endModel();
+    Node * addNode();
+    Connector * addConnector(const std::string & nodeName);
+    Connection * addConnection(const std::string & nodeAm, const std::string & nodeB);
     template<typename T>
-    T * select(const std::string & name){
-      return dynamic_cast<T*>(selectFirst([&name](T* element){element->name() == name;}));
-    }
+    T * select(const std::string & name);
 
-    ModelElement * element(const std::string  &name){
-      return Query<ModelElement*>::selectFirst(elements(),[&name](ModelElement* element){return element->name()==name;});
-    }
+    ModelElement * element(const std::string  &name);
   private:
-    
-    void addElement(ModelElement * element){
-      _currentElement = element;
-      Set<ModelElement*>::add(element);
-    }
 
+    void addElement(ModelElement * element);
   };
+
+  //template implementation
+  template<typename T>
+  T * ModelBuilderBase::select(const std::string & name){
+    return dynamic_cast<T*>(selectFirst([&name](T* element){element->name() == name;}));
+  }
+
+
 }
