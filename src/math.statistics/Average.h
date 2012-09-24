04144f01509ad6c6304df902d290b4c0119dc75f
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-09-24 15:33:15 +0000

    commented Average and IAverage
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@299 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- src/math.statistics/Average.h
+++ src/math.statistics/Average.h
@@ -4,25 +4,20 @@
 #include <math.statistics/IAverage.h>
 namespace nspace{
 
+// allows the calculation of the average for the last n samples (discards all older samples)
 class Average : IAverage{
   std::queue<Real> _queue;
   int _samples;
 public:
-  Average(int samples = 50):_samples(samples){}
-
-  void addSample(Real sample){
-    _queue.push(sample);
-  }
-
-  Real calculate(){
-    Real result = 0;
-    for(int i =0; i < _samples;i++){
-      Real val = _queue.front();
-      _queue.pop();
-      result+=val;
-      _queue.push(val);
-    }
-    return result / _samples;
-  }
+  // constructor. allows to specify how many samples should be averaged
+  Average(int samples = 50);
+  // access to the number of samples
+  int & numberOfSamples();
+  // readonly access to the number of samples
+  const int & numberOfSamples()const;
+  // add a sample
+  void addSample(Real sample);
+  // calculate the average
+  Real calculate();
 };
 }
--- src/math.statistics/IAverage.h
+++ src/math.statistics/IAverage.h
@@ -2,10 +2,13 @@
 #include <queue>
 #include <config.h>
 namespace nspace{
-class IAverage{
-public:
-  virtual void addSample(Real value)=0;
-  virtual Real calculate()=0;
-};
+  //interface for calulating an average.
+  class IAverage{
+  public:
+    // addsample method should be called for adding a new value
+    virtual void addSample(Real value)=0;
+    // calculate method should return the average which is to be caluclated
+    virtual Real calculate()=0;
+  };
 
 }
