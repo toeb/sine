486fb0739bf30e5b78fab3f648eab367f03133c5
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-10-24 12:31:21 +0000

    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@375 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- src/visualization.opengl/GlViewport.h
+++ src/visualization.opengl/GlViewport.h
@@ -24,7 +24,7 @@
 * \file src/vis/opengl/GlViewport.h
 */
 #pragma once
-#include <simulation.logging/LoggingObject.h>
+#include <simulation.logging/Log.h>
 #include <visualization/PerspectiveViewport.h>
 #include <core.initialization/Initializable.h>
 #include <visualization/Color.h>
