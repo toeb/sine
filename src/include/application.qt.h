2a522c71d698b8e1511f2bbe73978482c1c9db11
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-10-26 00:10:25 +0000

    added includes to combo include filed
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@395 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

---
+++ src/include/application.qt.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <application.qt/Plugin.h>
+#include <application.qt/PluginApplication.h>
+#include <application.qt/PluginContainer.h>
+#include <application.qt/PluginWindow.h>
+
