1f92e8228aa0465b77a52f8a262496fa035757d7
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-11-16 02:22:15 +0000

    added some fields to attributes
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@452 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- src/core/reflection/Attribute.h
+++ src/core/reflection/Attribute.h
@@ -5,7 +5,7 @@
 #include <core/Object.h>
 #include <math/Interval.h>
 namespace nspace{
-  
+
   class Attribute{
     TYPED_OBJECT(Attribute);
   };
@@ -14,23 +14,35 @@ namespace nspace{
     TYPED_OBJECT(DisplayAttribute);
     SUBCLASSOF(Attribute);
     SIMPLE_PROPERTY(std::string, DisplayName);
+    SIMPLE_PROPERTY(std::string, Description);
     SIMPLE_PROPERTY(std::string, GroupName);
     SIMPLE_PROPERTY(uint, Order);
   };
+  class StringAttribute:public virtual Attribute{
+    TYPED_OBJECT(StringAttribute);
+    SUBCLASSOF(Attribute);
+    SIMPLE_PROPERTY(int, MaximalLength){}
+    SIMPLE_PROPERTY(int, MinimalLength){}
+    SIMPLE_PROPERTY(std::string, Format){}
 
+  };
   class DecimalAttribute : public virtual Attribute{
     TYPED_OBJECT(DecimalAttribute);
     SUBCLASSOF(Attribute);
 
     SIMPLE_PROPERTY(Interval,Range){}
     SIMPLE_PROPERTY(double, StepSize){}
-    
+
   };
 
-  class NavigatableAttribute:
-    public virtual Attribute{
+  class NavigatableAttribute: public virtual Attribute{
     TYPED_OBJECT(NavigatableAttribute);
-    
+    SUBCLASSOF(Attribute);
+
   };
+
+
+
+#define ATTRIBUTE(TARGET, NAME, CUSTOMCODE) 
 
 }
