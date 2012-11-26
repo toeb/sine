277a969d0a44fa01ffa2506852310d58bbc5f0cb
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-11-26 01:04:54 +0000

    added a method to get the definition area of the piecewise function (but it does not work correclty yet)
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@474 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- src/math.functions/PiecewiseFunction.h
+++ src/math.functions/PiecewiseFunction.h
@@ -4,30 +4,31 @@
 namespace nspace{
 template <typename TY>
 class PiecewiseFunction : public Function<TY,Real>{
-  typedef std::pair<Interval,Function<TY,Real>*> Piece;
+    typedef std::pair<Interval,Function<TY,Real>*> Piece;
 private:
-  Set<Piece*> _Pieces;
-  
-public:
-   Set<Piece*> & Pieces(){return _Pieces;}
-   PiecewiseFunction(){
-     Piece * p=new Piece();
-     p->first=Interval();
-     p->second=0;
-     Pieces().add(p);
-   }
-  Piece * PiecePair(Real x){    
-    auto func = Pieces().first([x](Piece * pair){
-      return pair->first.a<=x && pair->first.b>x;
-    });   
-    return func;
-  }
-  Function<TY,Real>* PieceFunction(Real x){
-    auto p = PiecePair(x);
-    return p->second;  
-  }
+    Set<Piece*> _Pieces;
 
-  void add(Real xStart, Function<TY,Real>* func){
+public:
+    Set<Piece*> & Pieces(){return _Pieces;}
+    const Set<Piece*> & Pieces()const{return _Pieces;}
+    PiecewiseFunction(){
+        Piece * p=new Piece();
+        p->first=Interval();
+        p->second=0;
+        Pieces().add(p);
+    }
+    Piece * PiecePair(Real x){
+        auto func = Pieces().first([x](Piece * pair){
+                                   return pair->first.a<=x && pair->first.b>x;
+    });
+    return func;
+}
+Function<TY,Real>* PieceFunction(Real x){
+    auto p = PiecePair(x);
+    return p->second;
+}
+
+void add(Real xStart, Function<TY,Real>* func){
     auto p = PiecePair(xStart);
     if(!p)return;
     Piece * newPiece = new Piece;
@@ -39,14 +40,24 @@ public:
     //newPiece->first.b=
     
 
-  }
+}
 
-  inline bool evaluate(TY & result,  const Real & x){
+Interval getDefinitionInterval() const{
+    Interval interval(0,0);
+    for(int i=0; i < Pieces(); i++){
+        const Piece * piece = Pieces().at(i);
+        if(piece->first.a < interval.a)interval.a = piece->first.a;
+        if(piece->first.b > interval.b)interval.b = piece->first.b;
+    }
+    return interval;
+}
+
+inline bool evaluate(TY & result,  const Real & x){
     auto piece =PiecePair(x);
     if(!piece->second)return false;
     //Real deltaX = x-piece->first.a;
-    return piece->second->evaluate(result,x);    
+    return piece->second->evaluate(result,x);
     
-  }
+}
 };
 }
