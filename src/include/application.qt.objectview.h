873964250d70d7b78278c14d7b32676318843caf
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2012-11-26 01:07:35 +0000

    added an include statement
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@476 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- src/include/application.qt.objectview.h
+++ src/include/application.qt.objectview.h
@@ -4,6 +4,7 @@
 #include <application.qt.objectview/ObjectPropertyViewModel.h>
 #include <application.qt.objectview/ObjectTreeItem.h>
 #include <application.qt.objectview/ObjectViewPlugin.h>
+#include <application.qt.objectview/PropertyViewPlugin.h>
 #include <application.qt.objectview/PropertySetElementTreeItem.h>
 #include <application.qt.objectview/PropertySetTreeItem.h>
 #include <application.qt.objectview/PropertyTreeItem.h>
