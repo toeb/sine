55324df4f50bd99f5241d5a4a2e75a7d883fc3fe
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2013-01-10 15:15:19 +0000

    added download for ply model files
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@532 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

---
+++ resources/meshes/ply/download.cmd
@@ -0,0 +1,16 @@
+wget http://www.cc.gatech.edu/data_files/large_models/bunny.ply.gz
+wget http://www.cc.gatech.edu/data_files/large_models/happy.ply.gz
+wget http://www.cc.gatech.edu/data_files/large_models/dragon.ply.gz
+wget http://www.cc.gatech.edu/data_files/large_models/hand.ply.gz
+wget http://www.cc.gatech.edu/data_files/large_models/horse.ply.gz
+wget http://www.cc.gatech.edu/data_files/large_models/blade.ply.gz
+
+gzip -d bunny.ply.gz
+gzip -d happy.ply.gz
+gzip -d dragon.ply.gz
+gzip -d hand.ply.gz
+gzip -d horse.ply.gz
+gzip -d blade.ply.gz
+
+
+
