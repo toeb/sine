b783cfa8cea01f9c84959801f808b6d776dbe762
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2013-01-24 23:42:24 +0000

    added property field setter and test
    git-svn-id: https://subversion.assembla.com/svn/dsmks/trunk@577 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- src/test/core.reflection/reflection.cpp
+++ src/test/core.reflection/reflection.cpp
@@ -5,6 +5,7 @@
 
 #define DS_CURRENT_TYPE(CLASSNAME) typedef Person CurrentType;
 
+#define DS_INLINE inline
 
 
 #define DS_PROPERTY_NAME(NAME) NAME
@@ -25,50 +26,29 @@
 #define DS_PROPERTY_STORAGE_NAME(NAME) DS_CONCAT(_, DS_PROPERTY_NAME(NAME))
 
 #define DS_PROPERTY_STORAGE_TYPE_NAME(NAME) DS_PROPERTY_NAME(NAME)##StorageType
+
 #define DS_PROPERTY_STORAGE_TYPE_FIELD(NAME) typedef DS_PROPERTY_TYPE_NAME(NAME) DS_PROPERTY_STORAGE_TYPE_NAME(NAME);
 #define DS_PROPERTY_STORAGE_TYPE_POINTER(NAME) typedef std::add_pointer<DS_PROPERTY_TYPE_NAME(NAME)>::type DS_PROPERTY_STORAGE_TYPE_NAME(NAME);
 #define DS_PROPERTY_STORAGE_TYPE_REFERENCE(NAME) typedef std::add_reference<DS_PROPERTY_TYPE_NAME(NAME)>::type DS_PROPERTY_STORAGE_TYPE_NAME(NAME);
-
-
-
 
 #define DS_PROPERTY_STORAGE_FIELD(NAME) DS_PROPERTY_STORAGE_TYPE_FIELD(NAME) DS_PROPERTY_STORAGE_TYPE_NAME(NAME) DS_PROPERTY_STORAGE_NAME(NAME);
 #define DS_PROPERTY_STORAGE_POINTER(NAME)  DS_PROPERTY_STORAGE_TYPE_POINTER(NAME) DS_PROPERTY_STORAGE_TYPE_NAME(NAME) DS_PROPERTY_STORAGE_NAME(NAME);
 #define DS_PROPERTY_STORAGE_REFERENCE(NAME) DS_PROPERTY_STORAGE_TYPE_REFERENCE(NAME) DS_PROPERTY_STORAGE_TYPE_NAME(NAME) DS_PROPERTY_STORAGE_NAME(NAME);
 #define DS_PROPERTY_STORAGE_CALLBACK(NAME) 
 
-#define DS_PROPERTY_STORAGE(NAME)
+#define DS_PROPERTY_STORAGE(NAME) DS_PROPERTY_STORAGE_NAME(NAME)
 
-class Person{
-private: typedef Person CurrentType;
+#define DS_PROPERTY_GETTER_NAME(NAME) get##NAME
+#define DS_PROPERTY_SETTER_NAME(NAME) set##NAME
+#define DS_PROPERTY_GETTER_SIGNATURE(NAME) DS_INLINE const DS_PROPERTY_TYPE_NAME(NAME) & DS_PROPERTY_GETTER_NAME(NAME)()const
+#define DS_PROPERTY_SETTER_SIGNATURE(NAME) DS_INLINE void DS_PROPERTY_SETTER_NAME(NAME)(const DS_PROPERTY_TYPE_NAME(NAME) & value)
+#define DS_PROPERTY_SETTER_SIGNATURE_RVALUE(NAME) DS_INLINE void DS_PROPERTY_SETTER_NAME(NAME)(DS_PROPERTY_TYPE_NAME(NAME) && value)
 
+#define DS_PROPERTY_GETTER(NAME) DS_PROPERTY_GETTER_SIGNATURE(NAME)
 
+#define DS_PROPERTY_SETTER(NAME) DS_PROPERTY_SETTER_SIGNATURE(NAME)
 
-std::map<std::string, int> static const *__LastName(){return 0;}// this should not create any overhead and is possible for any type
-private: typedef std::remove_pointer<decltype(__LastName())>::type PropertyTypeLastName; //this is the definition of the property type
-private: PropertyTypeLastName LastName;
-
-private: int _Age;
-public: int getAge(){return _Age;}
-protected: void setAge(int & value){_Age=value;}
-protected: const int & age()const{return _Age;}
-
-bool DS_PROPERTY_DEFINITION(IsAlive);
-public: DS_PROPERTY_STORAGE_FIELD(IsAlive);
-
-        std::string DS_PROPERTY_DEFINITION(MiddleNames);
-      public: DS_PROPERTY_STORAGE_FIELD(MiddleNames);
-
-              Person DS_PROPERTY_DEFINITION(SpecialOther);
-public:   DS_PROPERTY_STORAGE_POINTER(SpecialOther);
-
-              Person DS_PROPERTY_DEFINITION(Self);
-public: DS_PROPERTY_STORAGE_REFERENCE(Self);
-public:
-  Person():_Self(*this),_IsAlive(false){}
-
-};
-
+#define DS_PROPERTY_SETTER_RVALUE(NAME) DS_PROPERTY_SETTER_SIGNATURE_RVALUE(NAME)
 
 TEST(1,propertyDefinition){
   struct A{
@@ -134,3 +114,56 @@ TTEST_DEFAULT(PropertyPointerStorage, typename PropertyType){
   res = std::is_same<A::ValueStorageType, std::add_pointer<PropertyType>::type>::value;
   if(!res) FAIL("the type of the storage field is incorrect");
 }
+
+
+TTEST_DEFAULT(PropertyReferenceStorage, typename PropertyType){
+  class A{
+    friend class UnitTestClass;
+    PropertyType DS_PROPERTY_DEFINITION(Value);
+    DS_PROPERTY_STORAGE_REFERENCE(Value);
+  };
+
+  auto res = std::is_reference<A::ValueStorageType>::value;
+  if(!res) FAIL("the property storage type is incorrect (not a pointer");
+  res = std::is_same<A::ValueStorageType, std::add_reference<PropertyType>::type>::value;
+  if(!res) FAIL("the type of the storage field is incorrect");
+}
+
+
+
+TEST(PropertyFieldSetter){
+  class A{
+    friend class UnitTestClass;
+    int DS_PROPERTY_DEFINITION(Value);
+    DS_PROPERTY_STORAGE_FIELD(Value);
+  public: DS_PROPERTY_SETTER(Value){DS_PROPERTY_STORAGE(Value)=value;}
+          
+
+  }a;
+  a.setValue(4);
+  auto val = a._Value;
+  CHECK_EQUAL(4,val);
+}
+/*
+template<typename TValue, typename TStorage>
+struct StorageAccess{
+  static inline void setStorage(){}
+  static inline TValue getStorage(){}
+};
+
+  class A{
+    int static const * ValueNullPointer(){return 0;} 
+    private: typedef std::remove_const<std::remove_pointer<decltype(ValueNullPointer())>::type>::type ValuePropertyType;;
+    typedef ValuePropertyType ValueStorageType; ValueStorageType _Value;;
+  private: static inline void assign(const ValuePropertyType & value){}
+  public: inline void inline void setValue(const ValuePropertyType & value){_Value=value;}
+          
+
+  }a;
+  */
+
+// int property(Count, storeAs(private: reference), defaultValue(0), public: getter, private: virtual setter, protected: const_reference )
+// propertyChanging(Count){}
+// propertyValidate(Count){ if (newvalue<0) return false;}
+// propertyChanged(Count){}
+// 
