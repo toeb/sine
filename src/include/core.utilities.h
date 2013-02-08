d7edaa9aef8d8a0768b4ada9a708fb5b691f80cc
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2013-02-08 16:20:15 +0000

    project generation works again
    git-svn-id: https://subversion.assembla.com/svn/dsmks/branches/core_refac@619 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- src/include/core.utilities.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <core/utilities/Helpers.h>
-#include <core/utilities/SourceInfo.h>
-#include <core/utilities/StringTools.h>
-#include <core/utilities/Timer.h>
-#include <core/utilities/Utilities.h>
-#include <core/utilities/ToString.h>
