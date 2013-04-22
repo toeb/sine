bd4a9263530dcfa062075d4b962b30bf7cf2c51b
toeb <toeb@c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56>
2013-04-22 11:12:38 +0000

    merge
    git-svn-id: https://subversion.assembla.com/svn/dsmks/branches/core_refac@679 c65b9dfa-2e9b-4d30-9b60-b5bbc3021b56

--- src/test/test.core.reflection/namespaceinfo.cpp
+++ src/test/test.core.reflection/namespaceinfo.cpp
@@ -2,9 +2,6 @@
 
 #include <core.reflection/namespace/NamespaceInfo.h>
 using namespace ds;
-
-
-
 UNITTEST(NamespaceConstruction1){
   struct Derived : public NamespaceInfo {
     Derived():NamespaceInfo("testnamespace"){
@@ -12,71 +9,33 @@ UNITTEST(NamespaceConstruction1){
     }
   }uut;
   CHECK_EQUAL("testnamespace",uut.getName());
-
-  CHECK_EQUAL("testnamespace",uut.getFullyQualifiedName());
-  CHECK_EQUAL(0,uut.getParentNamespace());
-  
-  CHECK(!contains(NamespaceInfo::Global()->Namespaces(), &uut));
-}
-
-UNITTEST(NamespaceConstruction2){
-  struct Derived : public NamespaceInfo {
-    Derived():NamespaceInfo("testnamespace"){
-      setScope(Global());
-    }
-  }uut;
-  CHECK_EQUAL("testnamespace",uut.getName());
-
-  CHECK_EQUAL("::testnamespace",uut.getFullyQualifiedName());
+  CHECK_EQUAL("::testnamespace::",uut.getFullyQualifiedName());
   CHECK_EQUAL(NamespaceInfo::Global(),uut.getParentNamespace());
-
-  CHECK(contains(NamespaceInfo::Global()->Namespaces(), &uut));
+  CHECK(NamespaceInfo::Global()->getNamespaces().contains(&uut));
 }
 
-UNITTEST(NamespaceConstruction3){
+UNITTEST(NamespaceConstruction2){  
+  auto glob = NamespaceInfo::Global();
+
+
   struct Derived1 : public NamespaceInfo{
-    Derived1():NamespaceInfo("test1"){
-      setScope(Global());
+    Derived1():NamespaceInfo("test1",Global()){
+
     }
     
   };
-
-  struct Derived2 : public NamespaceInfo{
-    Derived2(NamespaceInfo * ns):NamespaceInfo("test2"){
-      setScope(ns);
-    }
-  };
   Derived1 uut1;
-  Derived2 uut2(&uut1);
+  struct Derived2 : public NamespaceInfo{
+    Derived2(const NamespaceInfo * parent):NamespaceInfo("test2",parent){
 
-
-  auto glob = NamespaceInfo::Global();
+    }
+  }
+  uut2(&uut1);
   CHECK_EQUAL("test2",uut2.getName());
-  CHECK_EQUAL("::test1::test2",uut2.getFullyQualifiedName());
+  CHECK_EQUAL("::test1::test2::",uut2.getFullyQualifiedName());
   CHECK_EQUAL(&uut1,uut2.getParentNamespace());
-  CHECK(contains(uut1.Namespaces(),&uut2));
-  CHECK(!contains(NamespaceInfo::Global()->Namespaces(), &uut2));
+  CHECK(uut1.getNamespaces().contains(&uut2));
+  CHECK(!NamespaceInfo::Global()->getNamespaces().contains(&uut2));
 
 
-}
-
-UNITTEST(getSpecificNamespace1){
-  static auto global = NamespaceInfo::Global();
-
-  struct Derived1: public NamespaceInfo{
-    Derived1():NamespaceInfo("ns1"){setScope(NamespaceInfo::Global());}
-    ~Derived1(){}
-  }uut1;
-  struct Derived2:public NamespaceInfo{
-    Derived2(const NamespaceInfo & parent):NamespaceInfo("ns2"){setScope(&parent);}
-    ~Derived2(){}    
-
-  }uut2(uut1);
-
-  auto found = global->getNamespace("ns1::ns2");
-  CHECK_EQUAL(&uut2,found);
-  found = global->getNamespace("ns1");
-  CHECK_EQUAL(&uut1,found);
-  auto notfound = global->getNamespace("somenamespacename::that::does::not::exist::really");
-  CHECK_EQUAL(0,notfound);
 }
